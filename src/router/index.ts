54b77a5c263f7648ed56fbbddbf7a8648d9644b5
graphieros <alec.lloyd.probert@gmail.com>
2022-01-23 11:32:43 +0100

    adds generator route & page

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,11 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  {
+    path: "/generator",
+    name: "Generator",
+    component: () => import("../views/Generator.vue"),
+  },
 ];
 
 const router = createRouter({
