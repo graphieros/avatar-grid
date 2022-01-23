2b0cc41394c33d1bdef11f3a6f7a79626d261ede
graphieros <alec.lloyd.probert@gmail.com>
2022-01-23 11:31:44 +0100

    adds mdi font

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import WaveUI from "wave-ui";
 import "wave-ui/dist/wave-ui.css";
+import "@mdi/font/css/materialdesignicons.min.css";
 
 const app = createApp(App).use(router);
 
