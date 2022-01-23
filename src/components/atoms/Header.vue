8f0a5c7d4dc4e0821d516766a93f910d14d7e6f8
graphieros <alec.lloyd.probert@gmail.com>
2022-01-23 11:55:56 +0100

    adds a header to the generator page & todo features

---
+++ src/components/atoms/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import router from "../../router";
+import DarkModeSwitch from "./DarkModeSwitch.vue";
+
+function goHome() {
+  router.push("/");
+}
+</script>
+
+<template>
+  <header class="header">
+    <DarkModeSwitch toggleClass="header-switch" />
+    <h1 @click="goHome()" class="title white ml5 mt1">avatarGrid</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background: rgb(31, 31, 31);
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 60px;
+  width: 100%;
+  display: flex;
+  h1 {
+    cursor: pointer;
+  }
+}
+</style>
--- src/views/Generator.vue
+++ src/views/Generator.vue
@@ -1,5 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "../components/atoms/Header.vue";
+import { store } from "../store/index";
+import { computed } from "vue";
+
+const isDarkMode = computed<boolean>(() => {
+  return store.state.isDarkMode;
+});
+</script>
 
 <template>
-  <h1>GENERATOR</h1>
+  <Header />
+  <div :class="isDarkMode ? 'generator dark' : 'generator'">
+    <!-- place avatar render on the left, and choices on the right -->
+    <!-- find a cool way of clicking on an option and displaying the choices on right side drawer -->
+    <!-- save image as a feature -->
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.generator {
+  margin-top: 60px;
+  height: calc(100vh - 60px);
+}
+
+.dark {
+  background: rgb(46, 46, 46);
+  color: rgb(192, 192, 192);
+}
+</style>
